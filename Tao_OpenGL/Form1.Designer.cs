e22aa1615ae9a57bd90dfb4f3281f09926e26b0a
GameBoySystem <118464274+GameBoySystem@users.noreply.github.com>
2023-09-21 18:35:34 +0300

    Добавлена функциональность

--- Tao_OpenGL/Form1.Designer.cs
+++ Tao_OpenGL/Form1.Designer.cs
@@ -28,32 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.anT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.components = new System.ComponentModel.Container();
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.RenderTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             this.SuspendLayout();
             // 
-            // anT
+            // AnT
             // 
-            this.anT.AccumBits = ((byte)(0));
-            this.anT.AutoCheckErrors = false;
-            this.anT.AutoFinish = false;
-            this.anT.AutoMakeCurrent = true;
-            this.anT.AutoSwapBuffers = true;
-            this.anT.BackColor = System.Drawing.Color.Black;
-            this.anT.ColorBits = ((byte)(32));
-            this.anT.DepthBits = ((byte)(16));
-            this.anT.Location = new System.Drawing.Point(12, 12);
-            this.anT.Name = "anT";
-            this.anT.Size = new System.Drawing.Size(776, 355);
-            this.anT.StencilBits = ((byte)(0));
-            this.anT.TabIndex = 0;
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(12, 51);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(776, 355);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.AutoSize = true;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 373);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(167, 33);
             this.button1.TabIndex = 1;
@@ -65,7 +79,7 @@
             // 
             this.button2.AutoSize = true;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 415);
+            this.button2.Location = new System.Drawing.Point(185, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 33);
             this.button2.TabIndex = 2;
@@ -73,17 +87,128 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(804, 51);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 355);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.Value = 1000;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(866, 51);
+            this.trackBar2.Maximum = 1000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(56, 355);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.TickFrequency = 10;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(928, 51);
+            this.trackBar3.Maximum = 1000;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(56, 355);
+            this.trackBar3.TabIndex = 5;
+            this.trackBar3.TickFrequency = 10;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(800, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(862, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(924, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(800, 409);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(862, 409);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(924, 409);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "0";
+            // 
+            // RenderTimer
+            // 
+            this.RenderTimer.Interval = 25;
+            this.RenderTimer.Tick += new System.EventHandler(this.RenderTimer_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 455);
+            this.ClientSize = new System.Drawing.Size(1000, 446);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.anT);
+            this.Controls.Add(this.AnT);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,9 +216,19 @@
 
         #endregion
 
-        private Tao.Platform.Windows.SimpleOpenGlControl anT;
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer RenderTimer;
     }
 }
 
--- Tao_OpenGL/Form1.cs
+++ Tao_OpenGL/Form1.cs
@@ -18,39 +18,40 @@ namespace Tao_OpenGL
         public Form1()
         {
             InitializeComponent();
-            anT.InitializeContexts();
+            AnT.InitializeContexts();
         }
+
+        double a = 1, b = 0, c = 0;
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // инициализация библиотеки GLUT
             Glut.glutInit();
-            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
-            Gl.glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-            Gl.glViewport(0, 0, anT.Width, anT.Height);
+            // инициализация режима окна
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE |
+            Glut.GLUT_DEPTH);
+            // устанавливаем цвет очистки окна
+            Gl.glClearColor(255, 255, 255, 1);
+            // устанавливаем порт вывода, основываясь на размерах элемента управления AnT
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+            // устанавливаем проекционную матрицу
             Gl.glMatrixMode(Gl.GL_PROJECTION);
+            // очищаем ее
             Gl.glLoadIdentity();
-            Glu.gluPerspective(45, (float)anT.Width / (float)anT.Height, 0.1, 200);
+            // теперь необходимо корректно настроить 2D ортогональную проекцию
+            // в зависимости от того, какая сторона больше,
+            // мы немного варьируем конфигурацией настройки проекции
+            if (AnT.Width <= AnT.Height)
+                Glu.gluOrtho2D(0.0, 30.0, 0.0, 30.0 * (float)AnT.Height / (float)AnT.Width);
+            else
+                Glu.gluOrtho2D(0.0, 30.0 * (float)AnT.Width / (float)AnT.Height, 0.0, 30.0);
+            // переходим к объектно-видовой матрице
             Gl.glMatrixMode(Gl.GL_MODELVIEW);
-            Gl.glLoadIdentity();
-            Gl.glEnable(Gl.GL_DEPTH_TEST);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
-
-            Gl.glLoadIdentity();
-            Gl.glColor3f(0, 1.0f, 0);
-
-            Gl.glPushMatrix();
-            Gl.glTranslated(0, 0, -6);
-            Gl.glRotated(45, 1, 1, 0);
-
-            Glut.glutWireSphere(2, 48, 48);
-
-            Gl.glPopMatrix();
-            Gl.glFlush();
-            anT.Invalidate();
+            RenderTimer.Start();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -58,5 +59,65 @@ namespace Tao_OpenGL
             Application.Exit();
         }
 
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // генерация коэффициента
+            a = (double)trackBar1.Value / 1000.0;
+            // вывод значения коэффициента, управляемого данным ползунком.
+            // (под TrackBar'ом)
+            label4.Text = a.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            // генерация коэффициента
+            b = (double)trackBar2.Value / 1000.0;
+            // вывод значения коэффициента, управляемого данным ползунком.
+            // (под TrackBar'ом)
+            label5.Text = b.ToString();
+        }
+
+        private void RenderTimer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            // генерация коэффициента
+            c = (double)trackBar3.Value / 1000.0;
+            // вывод значения коэффициента, управляемого данным ползунком.
+            // (под TrackBar'ом)
+            label6.Text = c.ToString();
+        }
+
+        // функия Draw
+        private void Draw()
+        {
+            // очищаем буфер цвета
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            // активируем рисование в режиме GL_TRIANGLES, при котором три заданные
+            // с помощью функции glVertex2d или glVertex3d вершины
+            // будут объединяться в полигон (треугольник)
+            Gl.glBegin(Gl.GL_TRIANGLES);
+            // устанавливаем параметр цвета, основанный на параметрах a b c
+            Gl.glColor3d(0.0 + a, 0.0 + b, 0.0 + c);
+            // рисуем вершину в координатах 5,5
+            Gl.glVertex2d(5.0, 5.0);
+            // устанавливаем параметр цвета, основанный на параметрах с a b
+            Gl.glColor3d(0.0 + c, 0.0 + a, 0.0 + b);
+            // рисуем вершину в координатах 25,5
+            Gl.glVertex2d(25.0, 5.0);
+            // устанавливаем параметр цвета, основанный на параметрах b c a
+            Gl.glColor3d(0.0 + b, 0.0 + c, 0.0 + a);
+            // рисуем вершину в координатах 25,5
+            Gl.glVertex2d(5.0, 25.0);
+            // завершаем режим рисования примитивов
+            Gl.glEnd();
+            // дожидаемся завершения визуализации кадра
+            Gl.glFlush();
+            // обновляем изображение в элементе AnT
+            AnT.Invalidate();
+        }
     }
 }
