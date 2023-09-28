734eb18b64c1c80d8e302689b38073b57bc8aa98
GameBoySystem <118464274+GameBoySystem@users.noreply.github.com>
2023-09-28 17:44:11 +0300

    Добавлена функциональность

--- Tao_OpenGL/anEngine.cs
+++ Tao_OpenGL/anEngine.cs
@@ -110,6 +110,8 @@ namespace Tao_OpenGL
         private bool isVisible;
         // текущий установленный цвет 
         private Color ActiveColor;
+        // номер дисплейного списка для данного слоя
+        private int ListNom;
         // конструктор класса, в качестве входных параметров 
         // мы получаем размеры изображения, чтобы создать в памяти массив, 
         // который будет хранить растровые данные для данного слоя 
@@ -204,33 +206,76 @@ namespace Tao_OpenGL
             }
         }
         // функция визуализации слоя 
-        public void RenderImage()
+        public void RenderImage(bool FromList)
         {
-            // данную функцию мы улучшим в следующих частях,
-            // для того чтобы получить более быструю визуализацию, 
-            // но пока она будет выглядеть следующим образом 
-            // активируем режим рисования точек 
-            Gl.glBegin(Gl.GL_POINTS);
-            // проходим по всем точкам рисунка 
-            for (int ax = 0; ax < Width; ax++)
+            if (FromList)
+            // указана визуализация из дисплейного списка, следовательно данный слой не активен
             {
-                for (int bx = 0; bx < Heigth; bx++)
+                // вызываем дисплейный список 
+                Gl.glCallList(ListNom);
+            }
+            else
+            // данный слой активен, и визуализацию необходимо делать на ходу
+            {
+                // счетчик номеров элементов, которые должны участвовать в визуализации 
+                int count = 0;
+                // проходим по всем точкам рисунка 
+                for (int ax = 0; ax < Width; ax++)
                 {
-                    // если точка в координатах ax,bx не помечена флагом "прозрачная", 
-                    if (DrawPlace[ax, bx, 3] != 1)
+                    for (int bx = 0; bx < Heigth; bx++)
                     {
-                        // устанавливаем заданный в ней цвет 
-                        Gl.glColor3f(
-                            (float)(Convert.ToDouble(DrawPlace[ax, bx, 0]) / 255),
-                            (float)(Convert.ToDouble(DrawPlace[ax, bx, 1]) / 255),
-                            (float)(Convert.ToDouble(DrawPlace[ax, bx, 2]) / 255));
-                        // и выводим ее на экран 
-                        Gl.glVertex2i(ax, bx);
+                        // если точка в координатах ax,bx не помечена флагом "прозрачная" 
+                        if (DrawPlace[ax, bx, 3] != 1)
+                        {
+                            // не самый красивый способ, но так мы подсчитаем количество действительно значимых точек слоя, 
+                            // которые должны быть визуализированы 
+                            count++;
+                        }
                     }
                 }
+                // данный массив будет заполнен, а затем передан для быстрой отрисовки геометрии (точек в нашем случае) 
+                // колч. точек * 2 (для хранения координат x и y каждой точки, которая будет отрисована 
+                int[] arr_date_vertex = new int[count * 2];
+                // данный массив будет содержать значения цветов для всех отрисовываемых точек 
+                // колч. точек * 3 (для хранения координат R G B значений цветов каждой точки, которая будет отрисована) 
+                float[] arr_date_colors = new float[count * 3];
+                // счетчик элементов для создания массивов, которые будут переданы в реализацию OpenGL c 
+                // помощью функции glDrawArrays 
+                int now_element = 0;
+                // теперь, когда мы выделили массив необходимого размера, 
+                // мы заполним его необходимыми значениями 
+                for (int ax = 0; ax < Width; ax++)
+                {
+                    for (int bx = 0; bx < Heigth; bx++)
+                    {
+                        // если точка в координатах ax,bx не помечена флагом "прозрачная" 
+                        // если данная точка НЕ помечена флагом, сигнализирующим о том, что она не должна быть визуализирована 
+                        if (DrawPlace[ax, bx, 3] != 1)
+                        {
+                            // заносим координаты точки (ax , bx) в массив, который будет передан для визуализации 
+                            arr_date_vertex[now_element * 2] = ax; arr_date_vertex[now_element * 2 + 1] = bx;
+                            // заносим значения составляющих цвета, сразу перенося их в формат float 
+                            arr_date_colors[now_element * 3] = (float)DrawPlace[ax, bx, 0] / 255.0f;
+                            arr_date_colors[now_element * 3 + 1] = (float)DrawPlace[ax, bx, 1] / 255.0f;
+                            arr_date_colors[now_element * 3 + 2] = (float)DrawPlace[ax, bx, 2] / 255.0f;
+                            // подсчет добавленных элементов в массивы 
+                            now_element++;
+                        }
+                    }
+                }
+                // теперь, когда массивы с геометрическими данными и данными о цветах подготовлены, 
+                // включаем функцию использования массивов вершин и цветов 
+                Gl.glEnableClientState(Gl.GL_VERTEX_ARRAY); Gl.glEnableClientState(Gl.GL_COLOR_ARRAY);
+                // передаем массивы вершин и цветов, указывая количество элементов массива, приходящихся 
+                // на один визуализируемый элемент (в случае точек - 2 координаты: х и у, в случае цветов - 3 составляющие цвета) 
+                Gl.glColorPointer(3, Gl.GL_FLOAT, 0, arr_date_colors); Gl.glVertexPointer(2, Gl.GL_INT, 0, arr_date_vertex);
+                // вызываем функцию glDrawArrays которая позволит нам визуализировать наши массивы, передав их целиком, 
+                // а не передавая в цикле каждую точку
+                Gl.glDrawArrays(Gl.GL_POINTS, 0, count);
+                // деактивируем режим использования массивов геометрии и цветов 
+                Gl.glDisableClientState(Gl.GL_VERTEX_ARRAY);
+                Gl.glDisableClientState(Gl.GL_COLOR_ARRAY);
             }
-            // завершаем режим рисования 
-            Gl.glEnd();
         }
         // установка текущего цвета для рисования в слое
         public void SetColor(Color NewColor)
@@ -241,6 +286,33 @@ namespace Tao_OpenGL
         public Color GetColor()
         {
             return ActiveColor;
+        }
+        // функции удаления слоя 
+        public void ClearList()
+        {
+            // проверяем факт существования дисплейного списка с номером, хранимым в ListNom 
+            if (Gl.glIsList(ListNom) == Gl.GL_TRUE)
+            {
+                // удаляем его в случае существования 
+                Gl.glDeleteLists(ListNom, 1);
+            }
+        }
+        public void CreateNewList()
+        {
+            // проверяем факт существования дисплейного списка с номером, хранимым в ListNom 
+            if (Gl.glIsList(ListNom) == Gl.GL_TRUE)
+            {
+                // удаляем его в случае существования 
+                Gl.glDeleteLists(ListNom, 1);
+                // и генерируем новый номер 
+                ListNom = Gl.glGenLists(1);
+            }
+            // создаем дисплейный список 
+            Gl.glNewList(ListNom, Gl.GL_COMPILE);
+            // вызывая обычную визуализацию (не из списка) 
+            RenderImage(false);
+            // завершаем создание дисплейного списка 
+            Gl.glEndList();
         }
     }
 
@@ -285,9 +357,11 @@ namespace Tao_OpenGL
         // функция для установки номера активного слоя 
         public void SetActiveLayerNom(int nom)
         {
+            // текущий слой больше не будет активным, следовательно, надо создать новый дисплейный список для его быстрой визуализации 
+            ((anLayer)Layers[ActiveLayerNom]).CreateNewList();
             // новый активный слой получает установленный активный цвет для предыдущего активного слоя 
             ((anLayer)Layers[nom]).SetColor(((anLayer)Layers[ActiveLayerNom]).GetColor());
-            // установка номера активного слоя
+            // установка номера активного слоя 
             ActiveLayerNom = nom;
         }
         //установка видимости / невидимости слоя
@@ -304,9 +378,21 @@ namespace Tao_OpenGL
         // визуализация
         public void SwapImage()
         {
-            // вызываем функцию визуализации в нашем слое для всех существующих слоев 
+            // вызываем функцию визуализации в нашем слое 
             for (int ax = 0; ax < Layers.Count; ax++)
-                ((anLayer)Layers[ax]).RenderImage();
+            {
+                // если данный слой является активным в данный момент, 
+                if (ax == ActiveLayerNom)
+                {
+                    // вызываем визуализацию данного слоя напрямую 
+                    ((anLayer)Layers[ax]).RenderImage(false);
+                }
+                else
+                {
+                    // вызываем визуализацию слоя из дисплейного списка 
+                    ((anLayer)Layers[ax]).RenderImage(true);
+                }
+            }
         }
         // функция установки стандартной кисти, передается только размер 
         public void SetStandartBrush(int SizeB)
